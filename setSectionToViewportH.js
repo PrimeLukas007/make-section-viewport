73b9da63486a4f306d8287aa1e1133555999c6f9
Lukáš Jarmara <45052496+PrimeLukas007@users.noreply.github.com>
2024-02-10 14:50:02 +0100

    Update and rename squarespace QoL.js to setSectionToViewportH.js

---
+++ setSectionToViewportH.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const viewportHeight = window.innerHeight;
+    const sectionElement = document.querySelector('[data-section-id="YOUR-ID"]');
+    sectionElement.style.height = `${viewportHeight}px`;
+   
+});
